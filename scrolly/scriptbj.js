a2d5f93e4df883cc6b4c9b313c6ec8a22837c027
Jenny Jun Yi Zhang <55718366+jennyjyzhang@users.noreply.github.com>
2021-01-16 01:22:09 +0800

    more styling + switch to GMT+9

--- scrolly/scriptbj.js
+++ scrolly/scriptbj.js
@@ -4,8 +4,8 @@ var article = container.select('article');
 const stepSel = article.selectAll('.step');
 
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 30, left: 30},
-    width = window.innerWidth * 0.65,
+var margin = {top: 30, right: 80, bottom: 30, left: 80},
+    width = window.innerWidth * 0.8 - margin.left - margin.right,
     height = window.innerHeight * 0.8 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -17,7 +17,11 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("graphic.csv", function(data) {
+d3.selectAll("svg")
+	.style("background-color", "#2B2F77");
+
+
+d3.csv("bj.csv", function(data) {
 
     // List of groups (here I have one group per column)
     var allGroup = d3.map(data, function(d){return(d.npbysem)}).keys()
@@ -26,14 +30,39 @@ d3.csv("graphic.csv", function(data) {
 	var x = d3.scaleLinear()
 	    .domain([0, 24])     
 	    .range([0, width]);
-	svg.append("g")
-	    .attr("transform", "translate(0," + height + ")")
-	    .call(d3.axisBottom(x));
+
+	let xAxisGenerator = d3.axisBottom(x);
+	xAxisGenerator.ticks(25);
+	xAxisGenerator.tickSize(0);
+	xAxisGenerator.tickPadding(8);
+
+	let tickLabels = ['00:00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24:00 GMT+9'];
+	xAxisGenerator.tickFormat((d,i) => tickLabels[i]);
+
+	let xAxis =  svg.append("g")
+				.call(xAxisGenerator)
+				.attr("transform", "translate(0," + height + ")");
+
+	xAxis.selectAll(".tick text")
+		.attr("fill", "#D2D2D2")
+		.attr("font-size","13")
+		.attr("font-family","Roboto");
+
+	xAxis.select(".tick:nth-child(26)")
+		.selectAll("text")
+		.attr("dx", "25"); 
 
 	// Y axis: initialization
 	var y = d3.scaleLinear()
 	    .range([height, 0]);
-	var yAxis = svg.append("g")				
+
+	let yAxisGenerator = d3.axisLeft(y);
+	yAxisGenerator.tickSize(-width);
+	yAxisGenerator.tickPadding(8);
+
+	let yAxis = svg.append("g");		  	
+
+	yAxis.selectAll(".domain").remove()
 
 	// instantiate the scrollama
 	const scroller = scrollama();
@@ -78,13 +107,35 @@ d3.csv("graphic.csv", function(data) {
 	    var bins = histogram(datanew);
 
 	    // Y axis: update now that we know the domain
-	    if ((index == 3) || (index == 4)) { y.domain([0, 450]); }
-	    else { y.domain([0, 45]); }   // d3.hist has to be called before the Y axis obviously
+	    if (index == 3) { 
+	    	y.domain([0, 50]);
+	    	let tickLabels = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50 classes'];
+			yAxisGenerator.tickFormat((d,i) => tickLabels[i]);}
+	    else if (index == 4) { 
+	    	y.domain([0, 450]); 
+	    	let tickLabels = ['0', '50', '100', '150', '200', '250', '300', '350', '400', '450 classes'];
+			yAxisGenerator.tickFormat((d,i) => tickLabels[i]);}
+	    else { y.domain([0, 45]); 
+	    	let tickLabels = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45 classes'];
+			yAxisGenerator.tickFormat((d,i) => tickLabels[i]);}   // d3.hist has to be called before the Y axis obviously
 
 	    yAxis
-	        .transition()
-	        .duration(600)
-	        .call(d3.axisLeft(y));
+	    	.transition()
+	    	.duration(1000)
+	        .call(yAxisGenerator);
+
+    	yAxis
+    	.selectAll("g path")
+        .attr("opacity", "0");
+
+	    yAxis
+    	.selectAll(".tick text")
+        .attr("fill", "#D2D2D2")
+        .attr("font-size", "13");
+
+        yAxis
+    	.selectAll(".tick line")
+        .attr("stroke", "#D2D2D2");
 
 	    // Join the rect with the bins data
 	    var u = svg.selectAll("rect")
@@ -96,12 +147,13 @@ d3.csv("graphic.csv", function(data) {
 	        .append("rect") // Add a new rect for each new elements
 	        .merge(u) // get the already existing elements as well
 	        .transition() // and apply changes to all of them
-	        .duration(600)
+	        .duration(1000)
 	          .attr("x", 1)
 	          .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
 	          .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
 	          .attr("height", function(d) { return height - y(d.length); })
-	          .style("fill", "#69b3a2")
+	          .style("fill", "#5168A6")
+	          .attr("opacity", "90%")
 	}
 	
 
@@ -116,8 +168,8 @@ d3.csv("graphic.csv", function(data) {
 		// 3. bind scrollama event handlers (this can be chained like below)
 		scroller.setup({
 			step: '#scrolly-side article .step',
-			offset: 0.5,
-			debug: true
+			offset: 0.67,
+			debug: false
 		})
 		.onStepEnter(handleStepEnter)
 
