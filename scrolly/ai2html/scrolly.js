0ad2708c94c42e8ee1fce48935c4f6de426734e4
Jenny Jun Yi Zhang <55718366+jennyjyzhang@users.noreply.github.com>
2021-01-20 00:37:24 +0800

    cleaned scrolly + added fadein for class #s

--- scrolly/ai2html/scrolly.js
+++ scrolly/ai2html/scrolly.js
@@ -19,103 +19,16 @@ function handleResize() {
     scroller.resize();
 }
 
-function handleStepEnter(response) {
-    console.log("working");
-
-    if(response.index == 0){
-        d3.select(".step0").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 1){
-        d3.select(".step1").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 2){
-        d3.select(".step2").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 3){
-        d3.select(".step3").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 4){
-        d3.select(".step4").transition().duration(400).style("opacity", 1);
-    }
-    
-    if(response.index == 5){
-        d3.select(".step5").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 6){
-        d3.select(".step6").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 7){
-        d3.select(".step8").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 8){
-        d3.select(".step8").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 9){
-        d3.select(".step9").transition().duration(400).style("opacity", 1);
-    }
-
-    if(response.index == 10){
-        d3.select(".step10").transition().duration(400).style("opacity", 1);
-    }
+ function handleStepEnter(response) {
+    // add to color to current step
+    response.element.classList.add("is-active");
 }
 
-function handleStepExit(response) {
-    if (response.direction === "up") {
-
-        if(response.index == 0){
-        d3.select(".step0").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 1){
-            d3.select(".step1").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 2){
-            d3.select(".step2").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 3){
-            d3.select(".step3").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 4){
-            d3.select(".step4").transition().duration(400).style("opacity", 0);
-        }
-        
-        if(response.index == 5){
-            d3.select(".step5").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 6){
-            d3.select(".step6").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 7){
-            d3.select(".step8").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 8){
-            d3.select(".step8").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 9){
-            d3.select(".step9").transition().duration(400).style("opacity", 0);
-        }
-
-        if(response.index == 10){
-            d3.select(".step10").transition().duration(400).style("opacity", 0);
-        }
-    }
-
-
+ function handleStepExit(response) {
+    // add to color to current step
+   if (response.direction == "up") {
+        response.element.classList.remove("is-active");
+   }
 }
 
 function stepupStickyfill() {
